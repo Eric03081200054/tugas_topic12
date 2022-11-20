54bb37d4d2cff86835e89ef0d3ec4668c70af922
Eric <74457777+Eric03081200054@users.noreply.github.com>
2022-11-20 21:58:09 +0700

    ganti icon di sidebar

--- sisfoGAUPH/resources/views/layouts/sidebar.blade.php
+++ sisfoGAUPH/resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
     <li class="nav-item">
     <a href="/admin/category" class="nav-link">
-        <i class="nav-icon fas fa-fingerprint"></i>
+        <i class="nav-icon fas fa-align-justify"></i>
         <p>
         Categories
         </p>
@@ -9,7 +9,7 @@
     </li>
     <li class="nav-item">
         <a href="/admin/user-role" class="nav-link">
-            <i class="nav-icon fas fa-fingerprint"></i>
+            <i class="nav-icon fas fa-male"></i>
             <p>
             User Role
             </p>
@@ -17,7 +17,7 @@
     </li>
     <li class="nav-item">
         <a href="/admin/reservation_status" class="nav-link">
-            <i class="nav-icon fas fa-fingerprint"></i>
+            <i class="nav-icon fas fa-history"></i>
             <p>
             Reservation Status
             </p>
@@ -25,7 +25,7 @@
     </li>
     <li class="nav-item">
         <a href="/admin/missing_items_status" class="nav-link">
-            <i class="nav-icon fas fa-fingerprint"></i>
+            <i class="nav-icon fas fa-glasses"></i>
             <p>
             Missing Item Status
             </p>
@@ -33,7 +33,7 @@
     </li>
     <li class="nav-item">
         <a href="/admin/attendance" class="nav-link">
-            <i class="nav-icon fas fa-fingerprint"></i>
+            <i class="nav-icon fab fa-readme"></i>
             <p>
             Attendance
             </p>
@@ -41,7 +41,7 @@
     </li>
     <li class="nav-item">
         <a href="/admin/reservation" class="nav-link">
-            <i class="nav-icon fas fa-fingerprint"></i>
+            <i class="nav-icon fas fa-clipboard-list"></i>
             <p>
             Reservation
             </p>
@@ -49,7 +49,7 @@
     </li>
     <li class="nav-item">
         <a href="/admin/schedule" class="nav-link">
-            <i class="nav-icon fas fa-fingerprint"></i>
+            <i class="nav-icon fas fa-table"></i>
             <p>
             Schedule
             </p>
